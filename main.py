a4e646b9dbe1cdaf0735a6d05213cd2bb66e3e19
Shark-Firewall <Kumarrajnish363.rk@gmail.com>
2023-09-24 12:13:06 +0530

    Adding player on the window

--- main.py
+++ main.py
@@ -3,22 +3,37 @@ import pygame
 # Initilize the pygame
 pygame.init()
 
-# Creating the pygame window of size 500x500
+# Creating the pygame window of size 500x500 --> width x Height
 window = pygame.display.set_mode((500,500));
 
 # Overriding the default title of the window
-pygame.display.set_caption("First Pygame")
+pygame.display.set_caption("Space Invader")
 
 # Overriding the logo of the window --------
 icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
 
+# Adding player on the screen
+playerImg =  pygame.image.load("player.png")
+
+# Adding Player coordinate
+playerX = 230
+playerY = 400
+
+def Player():
+    window.blit(playerImg, (playerX, playerY))
+
+
+
 # Making sure the window running for a time until we close the window
 running = True;
 while running:
+
+    # Adding background color -- Here windom mean which window or screen we are targeting
+    window.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # Adding background color -- Here windom mean which window or screen we are targeting
-    window.fill((255,0,0))
+    Player();
     pygame.display.update()
